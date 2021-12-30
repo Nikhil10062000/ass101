1f126535f36404144d17564735c84b54b997a605
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2021-12-30 13:42:23 +0530

    Add files via upload

---
+++ ass101.js
@@ -0,0 +1,13 @@
+//W.A.P whether the number is greater than 10 or not 
+
+
+var num =prompt("Enter the Number");
+if (num>10)
+{ 
+alert("Number is greater than 10");
+}
+else { 
+alert("Number is not greater than 10");
+}
+
+
